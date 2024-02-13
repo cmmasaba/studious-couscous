abcb92b081dce6b3c543dd3b46c38fe71730d739
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-13 16:32:47 +0300

    Added custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class CustomUserModel(models.Model):
+    '''A customer user model for users.
+
+    Args:
+        models (Model): A model class from django.db.models
+    '''
+    user_code = models.UUIDField(primary_key=True, editable=False)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
+    password = models.CharField(max_length=100)
