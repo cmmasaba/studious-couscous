90810bfb39b4df89cb9c433013f6304b1a0cf87a
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-15 17:21:31 +0300

    Defined the view set for order objects

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from orders.models import Order
+from rest_framework import permissions
+from orders.permissions import IsOwnerOrReadOnly
+from rest_framework import viewsets
 
-# Create your views here.
+from orders.serializers import OrderSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides 'list', 'create', 'update',
+    'retrieve', and 'destroy' actions.
+    """
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+    
+    def perform_create(self, serializer):
+        """Associate the order with the customer."""
+        serializer.save(customer=self.request.user)
