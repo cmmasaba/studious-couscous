d18cb0a1ce924f317c3ee8c92c979ba06582917e
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-17 12:57:50 +0300

    Adjustments to the customers model

--- customers/models.py
+++ customers/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Customer(models.Model):
-    customer_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=50)
     phone = models.CharField(max_length=15)
+    code = models.CharField(max_length=10)
     
