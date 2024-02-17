5ee5bdbc8d48c41c40c8ed48732f2375daaeb1ad
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-17 10:43:08 +0300

    Modified the custom permissions class to allow only object owners

--- orders/permissions.py
+++ orders/permissions.py
@@ -1,13 +1,9 @@
 from rest_framework import permissions
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsOwner(permissions.BasePermission):
     """
     Custom permission to allow owners of an object to edit it.
     """
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request
-        # so we'll always allow GET, HEAD or OPTIONS requests.
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        # Write permissions are only allowed to the owner of the snippet.
+        # only owners of an object can edit it or view it.
         return obj.owner == request.user
