9f21e58f38fb5a95abfcefdb0d69172005a102b4
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-19 14:37:51 +0300

    Function that send sms to customers when an order is placed

---
+++ orders/send_sms.py
@@ -0,0 +1,39 @@
+import africastalking
+import environ
+
+env = environ.Env()
+
+africastalking.initialize(
+    username=env('AFT_USERNAME'),
+    api_key=env('AFT_API_KEY'),
+)
+
+class send():
+    sms = africastalking.SMS
+    def sending(self, recipients: list, message: str):
+        """Send an SMS to the recipient(s) with the message and sender name provided.
+        
+        Args:
+            recipient (list): A list of phone number(s) to send the message to.
+            message (str): The message to send.
+        """
+        
+        try:
+            # short code is 17117
+            response = self.sms.send(message, recipients, sender=17117)
+            status = response['SMSMessageData']['Recipients'][0]['status']
+            if status == 'Success':
+                print("Message sent successfully.")
+            else:
+                print(f"Message failed with error: {response}")
+        except Exception as e:
+            print(f"Encountered an error while sending: {e}")
+
+
+def send_sms(recipients: list, message: str):
+    """
+    Args:
+        recipient (list): A list of phone number(s) to send the message to.
+        message (str): The message to send.
+    """
+    send().sending(recipients, message)
