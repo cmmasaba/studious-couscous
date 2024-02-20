1757b70bac0090c21a5a8cdac529e8c589abe6c5
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-20 15:48:04 +0300

    Updated customers model

--- customers/models.py
+++ customers/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=50)
-    phone = models.CharField(max_length=15)
-    code = models.CharField(max_length=10)
+    first_name = models.CharField(max_length=20) # first name of the customer
+    last_name = models.CharField(max_length=20) # last name of the customer
+    phone = models.CharField(max_length=15, unique=True) # phone number of the customer
+    code = models.CharField(max_length=5, unique=True) # unique code for the customer
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} -: {self.code}"
     
