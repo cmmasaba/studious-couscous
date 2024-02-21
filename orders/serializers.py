2d4561b7af1f360f1849d87ce842cd97543b8926
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-21 08:14:31 +0300

    Updated the serializer to inclde the new fields

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,9 +3,9 @@ from rest_framework import serializers
 from orders.models import Order
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
-    customer = serializers.ReadOnlyField(source='customer.name')
-    item = serializers.ReadOnlyField(source='item.name')
+    customer = serializers.ReadOnlyField(source='customer.__str__')
     
     class Meta:
         model = Order
-        fields = ['url', 'id', 'time_placed', 'time_closed', 'quantity', 'status', 'customer', 'item', 'amount']
+        fields = ['id', 'item', 'amount', 'quantity', 'price_per_unit', 'status',
+                  'customer_code', 'customer','time_placed', 'url',]
