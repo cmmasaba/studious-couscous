46b67344982736203f13a8d6cfe6eac20223a238
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-21 12:53:06 +0300

    Removed unused imports and code

--- api_service/urls.py
+++ api_service/urls.py
@@ -16,28 +16,18 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.documentation import include_docs_urls
-from rest_framework.schemas import get_schema_view
-from rest_framework_swagger.views import get_swagger_view
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
-
-API_TITLE = 'Simple API Service for Customers and Orders'
-API_DESCRIPTION = 'A Web API for creating, modifying and deleting Customers and Orders'
-version='1.0.0'
-swagger_schema_view = get_swagger_view(title=API_TITLE)
-docs_schema_view = get_schema_view(title=API_TITLE,
-                                   description=API_DESCRIPTION,
-                                   version=version)
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # include urls for auhentication with mozilla-django-oidc
     path('api/v1/api-auth/', include('mozilla_django_oidc.urls')),
     path('api/v1/', include('api.urls')),
-    #path('api/schema', docs_schema_view, name='schema-view'),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
-    #path('api/docs/', include_docs_urls(title=API_TITLE, description=API_DESCRIPTION)),
-    # path('api/swagger-docs/', swagger_schema_view, name='swagger-docs'),
     path('api/schema/swagger-docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
