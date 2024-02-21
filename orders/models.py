80ebeda3ee557dfcf4124267517be6c3b670105e
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-21 08:12:46 +0300

    Updated the orders model

--- orders/models.py
+++ orders/models.py
@@ -12,14 +12,14 @@ ORDER_STATUS = {
 # Create your models here.
 class Order(models.Model):
     item = models.TextField()  # the item in the order
-    price = models.FloatField()  # the price of the item
+    price_per_unit = models.FloatField()  # the price of the item
     quantity = models.IntegerField()  # the number of items in the order
     status = models.CharField(choices=ORDER_STATUS, default='Pending',
                               max_length=10)  # the lifecycle of the order
     customer_code = models.CharField(max_length=5)  # the customer's code
     # time_placed is a required field and is automatically set to the current time when the order is created
     time_placed = models.DateTimeField(auto_now_add=True)
-    time_closed = models.DateTimeField()  # record when the order is closed
+    time_closed = models.DateTimeField(null=True, blank=True)  # record when the order is closed
 
     # create a many-to-one relationship with the custoners model. 
     # Many orders can belong to one customer
@@ -41,7 +41,7 @@ class Order(models.Model):
     def save(self, *args, **kwargs):
         """Override the save method to send an SMS when the order is placed."""
         self.customer = Customer.objects.get(code=self.customer_code)
-        send_sms([self.customer.phone_number], f"Greetings {self.customer.first_name}. \
+        send_sms([self.customer.phone], f"Greetings {self.customer.first_name}. \
                  Your order of {self.quantity} {self.item} has been placed at {self.time_placed}.\
                  Thank you for choosing SIL.")
         super().save(*args, **kwargs)
