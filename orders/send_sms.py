9c3007f2758675ed5dffbf8cc09270df4ba3b066
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-21 22:27:28 +0300

    Using http to send sms

--- orders/send_sms.py
+++ orders/send_sms.py
@@ -1,43 +1,15 @@
-import africastalking
-import environ
 import requests
+import environ
 
 env = environ.Env()
 
-africastalking.initialize(
-    username=env('AFT_USERNAME'),
-    api_key=env('AFT_API_KEY'),
-)
-
-class send():
-    sms = africastalking.SMS
-    def sending(self, recipients: list, message: str):
-        """Send an SMS to the recipient(s) with the message and sender name provided.
-        
-        Args:
-            recipient (list): A list of phone number(s) to send the message to.
-            message (str): The message to send.
-        """
-        
-        try:
-            # my Africa's Talking short code is 17117
-            response = self.sms.send(message, recipients, env('AFT_SHORT_CODE'))
-            status = response['SMSMessageData']['Recipients'][0]['status']
-            if status == 'Success':
-                print("Message sent successfully.")
-            else:
-                print(f"Message failed with error: {response}")
-        except Exception as e:
-            print(f"Encountered an error while sending: {e}")
-
-
+# using the http endpoint to send sms
 def send_sms(recipients: list, message: str):
     """
     Args:
         recipient (list): A list of phone number(s) to send the message to.
         message (str): The message to send.
     """
-    # send().sending(recipients, message)
     url = "https://api.sandbox.africastalking.com/version1/messaging"
     headers = {
         "apiKey": env('AFT_API_KEY'),
