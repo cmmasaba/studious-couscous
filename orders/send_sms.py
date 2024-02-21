6e2a1b1bc771b6780a80219a28d40362c5445c63
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-21 22:22:01 +0300

    Sending sms using http

--- orders/send_sms.py
+++ orders/send_sms.py
@@ -1,5 +1,6 @@
 import africastalking
 import environ
+import requests
 
 env = environ.Env()
 
@@ -36,4 +37,19 @@ def send_sms(recipients: list, message: str):
         recipient (list): A list of phone number(s) to send the message to.
         message (str): The message to send.
     """
-    send().sending(recipients, message)
+    # send().sending(recipients, message)
+    url = "https://api.sandbox.africastalking.com/version1/messaging"
+    headers = {
+        "apiKey": env('AFT_API_KEY'),
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+    }
+    data = {
+        "username": env('AFT_USERNAME'),
+        "to": recipients[0],
+        "message": message,
+        "from": env('AFT_SHORT_CODE')
+    }
+    response = requests.post(url, headers=headers, data=data)
+    with open ('smsresponse.txt', 'w') as file:
+        file.write(response.text)
