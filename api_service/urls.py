77df2a189c73f80a605c98b8e47f03ae53b10f1d
Collins Mmasaba <mmasabacollins9@gmail.com>
2024-02-22 13:48:59 +0300

    Testing deploy

--- api_service/urls.py
+++ api_service/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     # include urls for auhentication with mozilla-django-oidc
     path('api/v1/api-auth/', include('mozilla_django_oidc.urls')),
     path('api/v1/', include('api.urls')),
-    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'), # view schema
     path('api/v1/docs/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
     path('api/v1/swagger-docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
