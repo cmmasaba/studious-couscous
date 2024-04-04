e01be9610e99be2afc4f68e5096fb17267d63eb0
cmmasaba <mmasabacollins9@gmail.com>
2024-04-04 06:31:17 +0300

    Added owner field

--- orders/serializers.py
+++ orders/serializers.py
@@ -4,8 +4,19 @@ from orders.models import Order
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
     customer = serializers.ReadOnlyField(source='customer.__str__')
+    owner = serializers.ReadOnlyField(source='owner.username')
     
+    def validate_quantity(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Quantity must be  greater than 1")
+        return value
+    
+    def validate_price_per_unit(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Price per unit must be  greater than 1")
+        return value
+
     class Meta:
         model = Order
         fields = ['id', 'item', 'amount', 'quantity', 'price_per_unit', 'status',
-                  'customer_code', 'customer','time_placed', 'url',]
+                  'customer_code', 'customer','time_placed', 'owner']
