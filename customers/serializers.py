d9ca1ec46e38d55643f9c2144c78c2001e6d70ba
cmmasaba <mmasabacollins9@gmail.com>
2024-04-04 06:29:46 +0300

    Added owner field

--- customers/serializers.py
+++ customers/serializers.py
@@ -4,8 +4,9 @@ from customers.models import Customer
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     orders = serializers.HyperlinkedRelatedField(many=True, view_name='order-detail', read_only=True)
+    owner = serializers.ReadOnlyField(source='owner.username')
     url = serializers.HyperlinkedIdentityField(view_name='customer-detail', format='html')
 
     class Meta:
         model = Customer
-        fields = ['id', 'first_name', 'last_name','phone', 'code', 'orders', 'url',]
+        fields = ['id', 'first_name', 'last_name','phone', 'code', 'orders', 'url', 'owner']
