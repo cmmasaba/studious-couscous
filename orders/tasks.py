cf857841ee3ca5df9c86c88025c436fa86e291c9
cmmasaba <mmasabacollins9@gmail.com>
2025-02-20 12:28:52 +0300

    Celery task for sending sms

---
+++ orders/tasks.py
@@ -0,0 +1,29 @@
+import requests
+import environ
+from celery import shared_task
+
+env = environ.Env()
+
+# using the http endpoint to send sms
+@shared_task
+def send_sms(recipients: list, message: str):
+    """
+    Args:
+        recipient (list): A list of phone number(s) to send the message to.
+        message (str): The message to send.
+    """
+    url = "https://api.sandbox.africastalking.com/version1/messaging"
+    headers = {
+        "apiKey": env('AFT_API_KEY'),
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+    }
+
+    for recipient in recipients:
+        data = {
+            "username": env('AFT_USERNAME'),
+            "to": recipient,
+            "message": message,
+            "from": env('AFT_SHORT_CODE')
+        }
+        response = requests.post(url, headers=headers, data=data)
