06b50520b790b35bef1510bb2f7933003ebda426
cmmasaba <mmasabacollins9@gmail.com>
2025-02-23 08:21:50 +0300

    Updated views for customers

--- customers/views.py
+++ customers/views.py
@@ -1,9 +1,7 @@
 from customers.models import Customer
 from rest_framework import permissions
-from customers.permissions import IsOwner
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.reverse import reverse
+from customers.permissions import IsAuthorOrViewOnly
+
 from rest_framework import viewsets
 
 from customers.serializers import CustomerSerializer
@@ -16,21 +14,11 @@ class CustomerViewSet(viewsets.ModelViewSet):
     """
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    # only permit authenticated users and owners of the object
-    # permission_classes = [permissions.IsAuthenticated,
-    #                      IsOwner]
-    permission_classes = [permissions.IsAuthenticated,]
+    permission_classes = [
+        permissions.IsAuthenticated,
+        IsAuthorOrViewOnly
+    ]
     
-    def perform_create(self, serializer):
-        """Associate the customer with the user who created it."""
-        serializer.save(owner=self.request.user)
-
-
-"""Defning the api root
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'customers': reverse('customer-list', request=request, format=format),
-        'orders': reverse('order-list', request=request, format=format)
-    })
-"""
+    """def perform_create(self, serializer):
+        '''Associate the customer with the user who created it.'''
+        serializer.save(owner=self.request.user)"""
