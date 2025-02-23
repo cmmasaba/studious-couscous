1a04da1712077665d594d9ed921d530da3b710aa
cmmasaba <mmasabacollins9@gmail.com>
2025-02-23 09:05:30 +0300

    Added project-level settings for permissions

--- api_service/settings.py
+++ api_service/settings.py
@@ -172,6 +172,9 @@ REST_FRAMEWORK = {
         'mozilla_django_oidc.contrib.drf.OIDCAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'PAGE_SIZE': 10,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
