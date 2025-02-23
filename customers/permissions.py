9c434f4e545a323694d11193fd862dd3623e7895
cmmasaba <mmasabacollins9@gmail.com>
2025-02-23 08:20:37 +0300

    Updated permissions for customers

--- customers/permissions.py
+++ customers/permissions.py
@@ -1,9 +1,13 @@
 from rest_framework import permissions
 
-class IsOwner(permissions.BasePermission):
+class IsAuthorOrViewOnly(permissions.BasePermission):
     """
     Custom permission to allow owners of an object to edit it.
     """
     def has_object_permission(self, request, view, obj):
+        # Allow read-only permissions for any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
         # only owners of an object are allowed to edit it
-        return obj.owner == request.user
+        return obj.author == request.user
