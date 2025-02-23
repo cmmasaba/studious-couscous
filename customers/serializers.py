49210bc51b3ea35209655b0af1e0e828e66df11e
cmmasaba <mmasabacollins9@gmail.com>
2025-02-23 08:21:09 +0300

    Updated serializer for customers

--- customers/serializers.py
+++ customers/serializers.py
@@ -4,9 +4,9 @@ from customers.models import Customer
 
 class CustomerSerializer(serializers.HyperlinkedModelSerializer):
     orders = serializers.HyperlinkedRelatedField(many=True, view_name='order-detail', read_only=True)
-    owner = serializers.ReadOnlyField(source='owner.username')
+    author = serializers.ReadOnlyField(source='author.username')
     url = serializers.HyperlinkedIdentityField(view_name='customer-detail', format='html')
 
     class Meta:
         model = Customer
-        fields = ['id', 'first_name', 'last_name','phone', 'code', 'orders', 'url', 'owner']
+        fields = ['id', 'first_name', 'last_name','phone', 'code', 'orders', 'url', 'author']
