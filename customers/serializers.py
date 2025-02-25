01e113adec8faf288fff4463a0b17d771a5a80d4
cmmasaba <mmasabacollins9@gmail.com>
2025-02-25 19:56:29 +0300

    Modified the serializer

--- customers/serializers.py
+++ customers/serializers.py
@@ -2,11 +2,10 @@ from rest_framework import serializers
 
 from customers.models import Customer
 
-class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+class CustomerSerializer(serializers.ModelSerializer):
     orders = serializers.HyperlinkedRelatedField(many=True, view_name='order-detail', read_only=True)
-    author = serializers.ReadOnlyField(source='author.username')
     url = serializers.HyperlinkedIdentityField(view_name='customer-detail', format='html')
 
     class Meta:
         model = Customer
-        fields = ['id', 'first_name', 'last_name','phone', 'code', 'orders', 'url', 'author']
+        fields = ['id', 'first_name', 'last_name','phone', 'code', 'url', 'author']
