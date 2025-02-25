4b11f5a1c99db56cd76b3b1ef1eb80d23cfacdc3
cmmasaba <mmasabacollins9@gmail.com>
2025-02-25 19:57:55 +0300

    Modified serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -2,9 +2,8 @@ from rest_framework import serializers
 
 from orders.models import Order
 
-class OrderSerializer(serializers.HyperlinkedModelSerializer):
-    customer = serializers.ReadOnlyField(source='customer.__str__')
-    author = serializers.ReadOnlyField(source='author.username')
+class OrderSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='order-detail', format='html')
     
     def validate_quantity(self, value):
         if value <= 0:
@@ -19,4 +18,4 @@ class OrderSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Order
         fields = ['id', 'item', 'amount', 'quantity', 'price_per_unit', 'status',
-                  'customer_code', 'customer','time_placed', 'author']
+                  'customer_code', 'created_at', 'url', 'author']
